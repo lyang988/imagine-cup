da1c4367751552e2eba45d44f60d7375b58d8110
lydia <llyang@andrew.cmu.edu>
2023-01-21 12:16:14 -0500

    added ints lesson

--- app.js
+++ app.js
@@ -30,24 +30,24 @@ db.sequelize.authenticate().then(() => {
             }).then((lesson) => {
                 db.Page.create({
                     page: 1,
-                    pageData: { arr:
-                        [{"type": "regular", "text": "Here are two functions that print the bigger of the two arguments: (Top- Python, Bottom- Java)"},
-                {"iscode":true,"language": "python","type": "code", "text": "def print_bigger(a, b):\n    if a > b:\n        print(a)\n    else:\n        print(b)"},
-                {"iscode":true,"language": "java","type": "code", "text": "import java.io.*;\n\nvoid printBigger(int a, int b){\n    if (a > b) {\n        System.out.println(a); \n    } else {\n        System.out.println(b); \n    } \n}"},
-                {"type": "regular", "text": "Notice that the overall structure of the code is very similar, but the specific syntax is quite different. For instance, instead of using colons and indentation, Java uses curly brackets ("},
-                {"iscodetext":true,"language": "java","type": "codetext", "text": "{"},
-                {"type": "regular", "text": ", "},
-                {"iscodetext":true,"language": "java", "type": "codetext", "text": "}"},
-                {"type": "regular", "text": ") to specify function and if-statement bodies. Also, statements in Java end in a semicolon ("},
-                {"iscodetext":true,"language": "java","type": "codetext", "text": ";"},
-                {"type": "regular", "text": "). There are some other big differences that we will explore soon, but these two are the most visible - and easy to forget!\n "},
-                {"islinebreak": true, "type": "linebreak"},
-                {"isquestion": true, "questionId": 1},
-                {"isquestion": true, "questionId": 2},
-                {"istable":true, "type": "table", "array": [{" ":"Code Blocks and Control Flow","Python":"Use colons and indentation","Java":"Use opening and closing curly brackets: {, }"},
-                {" ":"Semicolons Ending Statements","Python":"Unnecessary (and proscribed)","Java":"Necessary"}
-                ]}
-            ]
+                    pageData: { arr: require("./test")
+            //             [{"type": "regular", "text": "Here are two functions that print the bigger of the two arguments: (Top- Python, Bottom- Java)"},
+            //     {"iscode":true,"language": "python","type": "code", "text": "def print_bigger(a, b):\n    if a > b:\n        print(a)\n    else:\n        print(b)"},
+            //     {"iscode":true,"language": "java","type": "code", "text": "import java.io.*;\n\nvoid printBigger(int a, int b){\n    if (a > b) {\n        System.out.println(a); \n    } else {\n        System.out.println(b); \n    } \n}"},
+            //     {"type": "regular", "text": "Notice that the overall structure of the code is very similar, but the specific syntax is quite different. For instance, instead of using colons and indentation, Java uses curly brackets ("},
+            //     {"iscodetext":true,"language": "java","type": "codetext", "text": "{"},
+            //     {"type": "regular", "text": ", "},
+            //     {"iscodetext":true,"language": "java", "type": "codetext", "text": "}"},
+            //     {"type": "regular", "text": ") to specify function and if-statement bodies. Also, statements in Java end in a semicolon ("},
+            //     {"iscodetext":true,"language": "java","type": "codetext", "text": ";"},
+            //     {"type": "regular", "text": "). There are some other big differences that we will explore soon, but these two are the most visible - and easy to forget!\n "},
+            //     {"islinebreak": true, "type": "linebreak"},
+            //     {"isquestion": true, "questionId": 1},
+            //     {"isquestion": true, "questionId": 2},
+            //     {"istable":true, "type": "table", "array": [{" ":"Code Blocks and Control Flow","Python":"Use colons and indentation","Java":"Use opening and closing curly brackets: {, }"},
+            //     {" ":"Semicolons Ending Statements","Python":"Unnecessary (and proscribed)","Java":"Necessary"}
+            //     ]}
+            // ]
                     },
                     lessonId: lesson.id
                 }).then((page) => {
